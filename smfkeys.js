948e11bd549ab0a714baf891b0180475db109731
Braden Shepherdson <braden.shepherdson@gmail.com>
2012-03-17 05:45:07 -0400

    Several changes and improvements. - Scroll to fit. - Set mode by URL. - Use jQuery properly.

--- smfkeys.js
+++ smfkeys.js
@@ -9,12 +9,21 @@ var States = {
 var SMFKeys = {};
 
 SMFKeys.state = States.OFF;
+SMFKeys.ready = false;
+SMFKeys.loaded = false;
 
 chrome.extension.sendRequest({ type: 'get', site: document.location.hostname }, handleGet);
 
 function handleGet(response) {
   SMFKeys.data = response;
-  focusRow();
+  SMFKeys.ready = true;
+  maybeInit();
+}
+
+function maybeInit() {
+  if(SMFKeys.ready && SMFKeys.loaded) {
+    focusRow();
+  }
 }
 
 function saveState() {
@@ -38,14 +47,24 @@ function keyPress(event) {
 
 // Returns the jQuery objects for all the rows in the current state.
 function getRows() {
-  if(SMFKeys.state = States.INDEX) {
+  if(SMFKeys.state == States.INDEX) {
     return $('tbody[class="content"] tr');
-  } else if(SMFKeys.state = States.BOARD) {
+  } else if(SMFKeys.state == States.BOARD) {
     return $('div[id="messageindex"] table tbody tr');
-  } else if(SMFKeys.state = States.THREAD) {
+  } else if(SMFKeys.state == States.THREAD) {
     return $('div[class="post_wrapper"]');
   } else {
     return [];
+  }
+}
+
+function getRow(index, f) {
+  if(SMFKeys.state == States.INDEX) {
+    $('tbody[class="content"] tr').eq(index).each(f);
+  } else if(SMFKeys.state == States.BOARD) {
+    $('div[id="messageindex"] table tbody tr').eq(index).each(f);
+  } else if(SMFKeys.state == States.THREAD) {
+    $('div[class="post_wrapper"]').eq(index).each(f);
   }
 }
 
@@ -56,9 +75,32 @@ function countRows() {
 
 // Focuses the currently selected row.
 function focusRow() {
-  var row = getRows()[SMFKeys.data[SMFKeys.state].position];
+  var index = SMFKeys.data[SMFKeys.state].position;
+  var row = getRows()[index];
   if(row) {
-    row.style.cssText = 'background: red';
+    if(SMFKeys.state == States.INDEX) {
+      row.style.cssText = 'background: red';
+    } else if(SMFKeys.state == States.BOARD) {
+      row.style.cssText = 'border-left: 3px solid red';
+    }
+
+    getRow(index, function() {
+      var row = $(this);
+      var rowTop = row.offset().top;
+      var rowHeight = row[0].offsetHeight;
+      var rowBottom = rowTop + rowHeight;
+      var windowHeight = $(window).height();
+
+      if(rowTop < window.scrollY) { // need to scroll up
+        window.scrollTo(0, rowTop);
+      } else if (rowBottom > window.scrollY + windowHeight) { // need to scroll down
+        if(rowHeight > windowHeight) { // doesn't fit, scroll to rowTop
+          window.scrollTo(0, rowTop);
+        } else { // does fit, scroll down to put it at the bottom.
+          window.scrollTo(0, rowBottom - windowHeight);
+        }
+      }
+    });
   }
 }
 
@@ -97,4 +139,17 @@ function moveUp() { // k
 
 $("document").ready(function() {
   document.addEventListener('keypress', keyPress, false);
+
+  // Determine state from the URL.
+  var path = document.location.search;
+  if(path.indexOf('board=') >= 0) {
+    SMFKeys.state = States.BOARD;
+  } else if(path.indexOf('topic=') >= 0) {
+    SMFKeys.state = States.THREAD;
+  } else if(document.location.pathname.match(/index.php$/) && !path) {
+    SMFKeys.state = States.INDEX;
+  }
+  SMFKeys.loaded = true;
+  maybeInit();
+  window.console.log(SMFKeys.state);
 });
